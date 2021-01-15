aa90ab78feae67375b7fe2057b4bd8b720629de8
Kars Mulder <devmail@karsmulder.nl>
2021-01-15 11:31:50 +0100

    Partially cleaned up src/bindings/libevdev.rs.

--- generate_bindings.sh
+++ generate_bindings.sh
@@ -2,4 +2,4 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 cat licenses/thirdparty/libevdev/header.txt | sed 's/^/\/\/ /' > src/bindings/libevdev.rs 
-bindgen src/bindings/libevdev.h -- -I"/usr/include/libevdev-1.0/" >> src/bindings/libevdev.rs
+bindgen --whitelist-type 'libevdev_.*' --whitelist-function 'libevdev_.*' --whitelist-var 'EV_.*' --whitelist-var 'REP_.*' src/bindings/libevdev.h -- -I"/usr/include/libevdev-1.0/" >> src/bindings/libevdev.rs
