24346621e8a0eb11ce85f28b7664bd6b1a51a2f2
Kars Mulder <devmail@karsmulder.nl>
2021-11-12 16:19:50 +0100

    --merge now rejects explicit non-EV_KEY filters.

--- src/arguments/merge.rs
+++ src/arguments/merge.rs
@@ -2,6 +2,7 @@
 
 use crate::error::ArgumentError;
 use crate::arguments::lib::ComplexArgGroup;
+use crate::event::EventType;
 use crate::key::{Key, KeyParser};
 use crate::merge::Merge;
 
@@ -28,7 +29,17 @@ impl MergeArg {
             namespace: crate::event::Namespace::User,
         };
 
-        let keys = parser.parse_all(&arg_group.get_keys_or_empty_key())?;
+        let mut keys: Vec<Key> = Vec::new();
+        for key_str in arg_group.get_keys_or_empty_key() {
+            let key = parser.parse(&key_str)?;
+            match key.requires_event_type() {
+                None | Some(EventType::KEY) => keys.push(key),
+                Some(_) => return Err(ArgumentError::new(format!(
+                    "The --merge argument is only applicable to EV_KEY type events (\"key:something\" or \"btn:something\"). As such, it does not make sense to give it the key \"{}\".",
+                    key_str
+                )))
+            }
+        }
 
         Ok(MergeArg { keys })
     }
@@ -36,4 +47,22 @@ impl MergeArg {
     pub fn compile(self) -> Merge {
         Merge::new(self.keys)
     }
+}
+
+#[test]
+fn unittest() {
+    assert!(MergeArg::parse(vec!["--merge".to_string()]).is_ok());
+    assert!(MergeArg::parse(vec!["--merge".to_string(), "".to_string()]).is_ok());
+    assert!(MergeArg::parse(vec!["--merge".to_string(), "key".to_string()]).is_ok());
+    assert!(MergeArg::parse(vec!["--merge".to_string(), "key:a".to_string()]).is_ok());
+    assert!(MergeArg::parse(vec!["--merge".to_string(), "btn".to_string()]).is_ok());
+    assert!(MergeArg::parse(vec!["--merge".to_string(), "btn:left".to_string()]).is_ok());
+    assert!(MergeArg::parse(vec!["--merge".to_string(), "key".to_string(), "btn".to_string()]).is_ok());
+    assert!(MergeArg::parse(vec!["--merge".to_string(), "@foo".to_string()]).is_ok());
+
+    assert!(MergeArg::parse(vec!["--merge".to_string(), "key:a:1".to_string()]).is_err());
+    assert!(MergeArg::parse(vec!["--merge".to_string(), "abs".to_string()]).is_err());
+    assert!(MergeArg::parse(vec!["--merge".to_string(), "abs:x".to_string()]).is_err());
+    assert!(MergeArg::parse(vec!["--merge".to_string(), "key".to_string(), "abs".to_string()]).is_err());
+    assert!(MergeArg::parse(vec!["--merge".to_string(), "abs@foo".to_string()]).is_err());
 }
