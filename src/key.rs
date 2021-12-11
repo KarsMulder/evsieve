a4df4aadd8e93216942d0330020610dce7e7fc0e
Kars Mulder <devmail@karsmulder.nl>
2021-12-11 16:14:32 +0100

    Interpret a factor of -d as -1d.

--- src/key.rs
+++ src/key.rs
@@ -476,15 +476,11 @@ fn interpret_relative_value(value_str: &str) -> Result<Option<KeyProperty>, Argu
     match value_str.strip_suffix('d') {
         None => Ok(None),
         Some(factor_str) => {
-            let factor: f64 = if factor_str.is_empty() {
-                1.0
-            } else {
-                match parse_number(factor_str) {
-                    Some(factor) => factor,
-                    None => return Err(ArgumentError::new(format!(
-                        "Cannot interpret {} as a float.", factor_str
-                    ))),
-                }
+            let factor = match parse_number(factor_str) {
+                Some(factor) => factor,
+                None => return Err(ArgumentError::new(format!(
+                    "Cannot interpret {} as a float.", factor_str
+                ))),
             };
             Ok(Some(KeyProperty::DeltaFactor(factor)))
         }
@@ -492,11 +488,16 @@ fn interpret_relative_value(value_str: &str) -> Result<Option<KeyProperty>, Argu
 }
 
 /// Like str::parse, but a bit more stringent, in that it also rejects floats containing e+ or NaN.
+/// Also interprets "" as 1.0 and "-" as -1.0.
 fn parse_number(string: &str) -> Option<f64> {
     const ALLOWED_CHARS: &str = "0123456789-.";
-    if string.chars().all(|c| ALLOWED_CHARS.contains(c)) {
-        string.parse().ok()
-    } else {
-        None
+    match string {
+        "-" => Some(-1.0),
+        "" => Some(1.0),
+        _ => if string.chars().all(|c| ALLOWED_CHARS.contains(c)) {
+            string.parse().ok()
+        } else {
+            None
+        }
     }
 }
