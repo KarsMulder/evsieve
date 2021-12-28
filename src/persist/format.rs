66f47a24353003c1cf8c23d098a176cc1cdb946c
Kars Mulder <devmail@karsmulder.nl>
2021-12-28 13:45:52 +0100

    More work on capabilities file parsing.

--- src/persist/format.rs
+++ src/persist/format.rs
@@ -20,11 +20,12 @@
 // In the special case of EV_ABS type capabilities, they must be additional axis information specified
 // in the following way:
 //
-// abs:CODE (min=INT, max=INT, fuzz=INT, flat=INT, resolution=INT)
+//   abs:CODE (min=INT, max=INT, fuzz=INT, flat=INT, resolution=INT)
 //
+// This line must start with two spaces.
 // EV_REP capabilities must be encoded as follows:
 //
-// rep (delay=UINT16, period=UINT16)
+//   rep (delay=UINT16, period=UINT16)
 // 
 // All the above fields must be provided in the exact order as specified above. The file must end
 // with a newline (\n) character. An example valid file is shown below:
@@ -37,11 +38,11 @@
 // Path: /dev/input/by-id/my\nescaped\\path
 //
 // Capabilities:
-// key:a
-// key:b
-// key:c
-// abs:x (min=-5, max=5, fuzz=2, flat=1, resolution=1)
-// rep (delay=250, period=33)
+//   key:a
+//   key:b
+//   key:c
+//   abs:x (min=-5, max=5, fuzz=2, flat=1, resolution=1)
+//   rep (delay=250, period=33)
 // ```
 
 use std::fmt::Write;
@@ -54,6 +55,10 @@ impl ParseError {
     pub fn new(message: String) -> ParseError {
         ParseError { message }
     }
+
+    pub fn unexpected_eof() -> ParseError {
+        ParseError::new("Unexpected EOF.".to_owned())
+    }
 }
 
 /// This and its twin `unescape_path()` are for escaping the path to input files so they never contain
@@ -63,7 +68,7 @@ fn escape_path(path: String) -> String {
         .replace("\n", "\\n")
 }
 
-fn unescape_path(path: String) -> Result<String, ParseError> {
+fn unescape_path(path: &str) -> Result<String, ParseError> {
     let mut iter = path.chars();
     let mut result = String::new();
     while let Some(character) = iter.next() {
@@ -88,9 +93,9 @@ fn format_device_path(path: String) -> String {
     result
 }
 
-fn parse_device_path(path_line: String) -> Result<String, ParseError> {
+fn parse_device_path(path_line: &str) -> Result<String, ParseError> {
     let path = match path_line.strip_prefix("Path: ") {
-        Some(path_escaped) => unescape_path(path_escaped.to_string())?,
+        Some(path_escaped) => unescape_path(&path_escaped)?,
         None => return Err(ParseError::new(format!(
             "Expected \"Path: something\", encountered: \"{}\"", path_line
         ))),
@@ -107,6 +112,7 @@ fn parse_device_path(path_line: String) -> Result<String, ParseError> {
 
 const MAGICAL_NUMBER_HEADER: &str = "Evsieve event device capabilities description file";
 const FORMAT_VERSION_HEADER: &str = "Format version: 1.0";
+const CAPABILITIES_HEADER: &str = "Capabilities:";
 const EMPTY_LINE: &str = "";
 
 fn write() -> Result<String, std::fmt::Error> {
@@ -116,5 +122,35 @@ fn write() -> Result<String, std::fmt::Error> {
     writeln!(output, "{}", EMPTY_LINE)?;
     writeln!(output, "{}", format_device_path(unimplemented!()))?;
     writeln!(output, "{}", EMPTY_LINE)?;
+    writeln!(output, "{}", CAPABILITIES_HEADER)?;
     Ok(output)
+}
+
+/// A helper function for parsing. If the input is not identical to the required value, returns
+/// a ParseError.
+fn expect_equal(input: &str, expected_value: &str) -> Result<(), ParseError> {
+    if input == expected_value {
+        Ok(())
+    } else {
+        Err(ParseError::new(format!("Expected: {}, received: {}", expected_value, input)))
+    }
+}
+
+/// Like `str::strip_prefix`, except it returns a ParseError if the string does not start with
+/// the prefix instead of returning None.
+fn expect_strip_prefix<'a>(input: &'a str, expected_prefix: &str) -> Result<&'a str, ParseError> {
+    input.strip_prefix(expected_prefix).ok_or_else(|| ParseError::new(format!(
+        "Expecte: {}, received: {}", expected_prefix, input
+    )))
+}
+
+fn read(input: &str) -> Result<(), ParseError> {
+    let mut lines = input.lines();
+    expect_equal(lines.next().ok_or_else(ParseError::unexpected_eof)?, MAGICAL_NUMBER_HEADER)?;
+    expect_equal(lines.next().ok_or_else(ParseError::unexpected_eof)?, FORMAT_VERSION_HEADER)?;
+    expect_equal(lines.next().ok_or_else(ParseError::unexpected_eof)?, EMPTY_LINE)?;
+    let path = parse_device_path(lines.next().ok_or_else(ParseError::unexpected_eof)?)?;
+    expect_equal(lines.next().ok_or_else(ParseError::unexpected_eof)?, EMPTY_LINE)?;
+    expect_equal(lines.next().ok_or_else(ParseError::unexpected_eof)?, CAPABILITIES_HEADER)?;
+    unimplemented!();
 }
