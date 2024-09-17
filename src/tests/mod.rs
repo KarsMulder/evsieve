9ae458397676550403bb34ea3143ae32278f8f8f
Kars Mulder <devmail@karsmulder.nl>
2024-09-17 21:22:56 +0200

    Add more unittests for --withhold.

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -135,6 +135,138 @@ fn test_withhold_with_breaks_on() {
 }
 
 #[test]
+fn test_withhold_sending_its_own_keys() {
+    run_test(
+        // Arguments
+        "
+        --hook key:a key:b send-key=key:a
+        --withhold
+        ",
+        // Input
+        "
+        key:a:1 key:a:0
+        key:b:1 key:b:0
+        key:x:1 key:x:0
+
+        key:a:1 key:b:1 key:a:0 key:b:0
+        key:y:1 key:y:0
+        key:b:1 key:a:1 key:b:0 key:a:0
+        key:y:1 key:y:0
+        ",
+        // Output
+        "
+        key:a:1 key:a:0
+        key:b:1 key:b:0
+        key:x:1 key:x:0
+
+        key:a:1 key:a:0
+        key:y:1 key:y:0
+        key:a:1 key:a:0
+        key:y:1 key:y:0
+        "
+    )
+}
+
+#[test]
+fn test_withhold_sending_keys_for_later_hooks() {
+    run_test(
+        // Arguments
+        "
+        --hook key:a key:b send-key=key:s
+        --hook key:s key:t send-key=key:f
+        --withhold
+        ",
+        // Input
+        "
+        key:s:1 key:s:0
+        key:s:1 key:t:1 key:s:0 key:t:0
+        key:x:1 key:x:0
+
+        key:s:1 key:a:1 key:s:0 key:a:0
+        key:y:1 key:y:0
+
+        key:s:1 key:a:1 key:b:1 key:s:0 key:a:0 key:b:0
+        key:z:1 key:z:0
+        key:t:1 key:a:1 key:b:1 key:t:0 key:a:0 key:b:0
+        key:z:1 key:z:0
+
+        key:x:1 key:a:1 key:b:1 key:x:0 key:a:0 key:b:0
+        key:y:1 key:y:0
+
+        key:s:1 key:a:1 key:b:1 key:t:1 key:s:0 key:a:0 key:b:0 key:t:0
+        key:z:1 key:z:0
+        ",
+        // Output
+        "
+        key:s:1 key:s:0
+        key:f:1 key:f:0
+        key:x:1 key:x:0
+
+        key:s:1 key:s:0 key:a:1 key:a:0
+        key:y:1 key:y:0
+
+        key:s:1 key:s:1 key:s:0 key:s:0
+        key:z:1 key:z:0
+        key:f:1 key:f:0
+        key:z:1 key:z:0
+
+        key:x:1 key:x:0 key:s:1 key:s:0
+        key:y:1 key:y:0
+
+        key:f:1 key:f:0
+        key:z:1 key:z:0
+        "
+    )
+}
+
+#[test]
+fn test_withhold_sending_keys_for_later_hooks_2() {
+    run_test(
+        // Arguments
+        "
+        --hook key:a key:b
+        --hook key:s send-key=key:a
+        --hook key:a key:c send-key=key:f
+        --withhold
+        ",
+        // Input
+        "
+        key:a:1 key:a:0 key:x:1 key:x:0
+        key:b:1 key:b:0 key:x:1 key:x:0
+        key:c:1 key:c:0 key:x:1 key:x:0
+        key:s:1 key:s:0 key:x:1 key:x:0
+        
+        key:s:1 key:c:1 key:s:0 key:c:0 key:x:1 key:x:0
+        key:c:1 key:s:1 key:c:0 key:s:0 key:y:1 key:y:0
+        key:s:1 key:b:1 key:s:0 key:b:0 key:x:1 key:x:0
+        key:b:1 key:s:1 key:b:0 key:s:0 key:y:1 key:y:0
+        key:a:1 key:b:1 key:a:0 key:b:0 key:x:1 key:x:0
+        key:b:1 key:a:1 key:b:0 key:a:0 key:y:1 key:y:0
+
+        key:a:1 key:b:1 key:c:1 key:a:0 key:b:0 key:c:0
+        key:a:1 key:a:0
+        ",
+        // Output
+        "
+        key:a:1 key:a:0 key:x:1 key:x:0
+        key:b:1 key:b:0 key:x:1 key:x:0
+        key:c:1 key:c:0 key:x:1 key:x:0
+        key:a:1 key:a:0 key:x:1 key:x:0
+
+        key:f:1 key:f:0 key:x:1 key:x:0
+        key:f:1 key:f:0 key:y:1 key:y:0
+        key:a:1 key:a:0 key:b:1 key:b:0 key:x:1 key:x:0
+        key:b:1 key:b:0 key:a:1 key:a:0 key:y:1 key:y:0
+        key:x:1 key:x:0
+        key:y:1 key:y:0
+
+        key:f:1 key:f:0
+        key:a:1 key:a:0
+        "
+    )
+}
+
+#[test]
 fn test_withhold_for_channelless_hooks() {
     // The supposed outcome may look unintuitive, but it is the correct one because a tracker
     // of a hook is documented to deactivate when an event with a value not in the range 1~
