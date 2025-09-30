b67f32c355eed2e9424aa26d10870700d6c2b3ff
Kars Mulder <devmail@karsmulder.nl>
2025-09-30 21:09:53 +0200

    Update libevdev bindings.

--- generate_bindings.sh
+++ generate_bindings.sh
@@ -2,11 +2,11 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 cat licenses/thirdparty/libevdev/header.txt | sed 's/^/\/\/ /' > src/bindings/libevdev.rs 
-bindgen --whitelist-type 'libevdev_.*' \
-        --whitelist-function 'libevdev_.*' \
-        --whitelist-var 'EV_.*' \
-        --whitelist-var 'REP_.*' \
-        --whitelist-var 'MSC_.*' \
+bindgen --allowlist-type 'libevdev_.*' \
+        --allowlist-function 'libevdev_.*' \
+        --allowlist-var 'EV_.*' \
+        --allowlist-var 'REP_.*' \
+        --allowlist-var 'MSC_.*' \
         --no-doc-comments \
         src/bindings/libevdev.h \
         -- -I"/usr/include/libevdev-1.0/" \
