230c8e68c7c54a7ea25bbcbf4373a3d9818597b7
Kars Mulder <devmail@karsmulder.nl>
2025-09-30 19:26:25 +0200

    Add CapabilityOverrideSet abstraction.

--- src/arguments/capability.rs
+++ src/arguments/capability.rs
@@ -1,3 +1,4 @@
+
 use std::fmt::Write;
 use std::collections::HashMap;
 
@@ -12,7 +13,7 @@ use super::lib::ComplexArgGroup;
 
 /// Represents a --capability argument.
 pub(super) struct CapabilityArg {
-    pub overrides: HashMap<EventCode, CapabilityOverrideSpec>,
+    pub overrides: CapabilityOverrideSet,
 }
 
 #[derive(Clone, Copy)]
@@ -21,6 +22,60 @@ pub struct CapabilityOverrideSpec {
     pub flat: Option<i32>,
     pub fuzz: Option<i32>,
     pub value: Option<i32>,
+}
+
+pub struct CapabilityOverrideSet {
+    pub inner: HashMap<EventCode, CapabilityOverrideSpec>,
+}
+
+/// Returned when incompatible capability overrides are specified.
+pub struct IncompatibleError;
+
+impl CapabilityOverrideSet {
+    pub fn new() -> Self {
+        Self { inner: HashMap::new() }
+    }
+
+    /// Adds a new specification to this set, unless a confliction specification already exists.
+    pub fn try_add(&mut self, code: EventCode, additional_spec: CapabilityOverrideSpec) -> Result<(), IncompatibleError> {
+        use std::collections::hash_map::Entry;
+        match self.inner.entry(code) {
+            Entry::Occupied(mut occupied_entry) => {
+                let current_spec: CapabilityOverrideSpec = *occupied_entry.get();
+                *occupied_entry.get_mut() = current_spec.try_merge_with(additional_spec)?;
+                Ok(())
+            },
+            Entry::Vacant(vacant_entry) => {
+                vacant_entry.insert(additional_spec);
+                Ok(())
+            },
+        }
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = (&EventCode, &CapabilityOverrideSpec)> {
+        self.inner.iter()
+    }
+}
+
+impl CapabilityOverrideSpec {
+    /// Returns an override that specifies the requirements from both specifications. Returns an error if the specifications
+    /// are incompatible (e.g. both require a different range.)
+    pub fn try_merge_with(self, other: CapabilityOverrideSpec) -> Result<CapabilityOverrideSpec, IncompatibleError> {
+        fn merge_options<T: Eq>(opt_1: Option<T>, opt_2: Option<T>) -> Result<Option<T>, IncompatibleError> {
+            match (opt_1, opt_2) {
+                (None, None) => Ok(None),
+                (None, Some(val)) | (Some(val), None) => Ok(Some(val)),
+                (Some(val_1), Some(val_2)) => if val_1 == val_2 { Ok(Some(val_1)) } else { Err(IncompatibleError) },
+            }
+        }
+
+        Ok(CapabilityOverrideSpec {
+            range: merge_options(self.range, other.range)?,
+            flat:  merge_options(self.flat, other.flat)?,
+            fuzz:  merge_options(self.fuzz, other.fuzz)?,
+            value: merge_options(self.value, other.value)?,
+        })
+    }
 }
 
 
@@ -59,7 +114,7 @@ impl CapabilityArg {
         let fuzz: Option<i32> = arg_group.get_unique_clause_i32(FUZZ)?;
         let initial_value: Option<i32> = arg_group.get_unique_clause_i32(VALUE)?;
 
-        let mut overrides = HashMap::new();
+        let mut overrides = CapabilityOverrideSet::new();
         for key_str in &arg_group.keys {
             let key_parse_context_msg = || format!("While parsing the key \"{}\":", key_str);
             let key = parser.parse(&key_str)?;
@@ -94,15 +149,17 @@ impl CapabilityArg {
                 }
             }
 
-            overrides.insert(code, CapabilityOverrideSpec {
+            if overrides.try_add(code, CapabilityOverrideSpec {
                 range: range_opt,
                 flat, fuzz, value: initial_value,
-            });
+            }).is_err() {
+                return Err(ArgumentError::new(format!("The capability override {} is incompatible with a previously specified capability.", key_str)));
+            };
         }
 
         // Sanity check: only allow the clauses flat, fuzz and value if at least one of the specified capabilities
         // is an EV_ABS capability.
-        if overrides.keys().any(|key| key.ev_type().is_abs()) {
+        if ! overrides.inner.keys().any(|key| key.ev_type().is_abs()) {
             let improper_clauses = IntoIterator::into_iter([FLAT, FUZZ, VALUE])
                     .filter(|clause| !arg_group.get_clauses(clause).is_empty())
                     .collect::<Vec<_>>();
@@ -124,6 +181,6 @@ impl CapabilityArg {
     }
 
     pub fn compile(self, device: Domain) -> CapabilityOverride {
-        CapabilityOverride::new(device, self.overrides)
+        CapabilityOverride::new(device, self.overrides.inner)
     }
 }
